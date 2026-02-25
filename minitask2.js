1182f5ffd783f7af6863396da73d5316c365cbc8
M. Hanif Irfan <mhirfan173@gmail.com>
2026-02-25 14:43:52 +0700

    Delete minitask2.js

--- minitask2.js
+++
@@ -1,8 +0,0 @@
-let belanja = 700000
-if (belanja > 1000000) {
-    console.log("Diskon 10%")
-} else if (belanja >= 500000 && belanja <= 1000000){
-    console.log("Diskon 5%")
-} else {
-    console.log("Tidak ada diskon")
-}
