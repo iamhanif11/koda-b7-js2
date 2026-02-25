3c08873d0c31310c51cff4db9692f70254bb0b01
M. Hanif Irfan <mhirfan173@gmail.com>
2026-02-25 14:27:44 +0700

    Add files via upload

---
+++ minitask2.js
@@ -0,0 +1,8 @@
+let belanja = 700000
+if (belanja > 1000000) {
+    console.log("Diskon 10%")
+} else if (belanja >= 500000 && belanja <= 1000000){
+    console.log("Diskon 5%")
+} else {
+    console.log("Tidak ada diskon")
+}
